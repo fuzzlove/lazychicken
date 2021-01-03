e43426f4bef6d31a285bfb1b2b296fd4ccb8b65e
Joseph McPeters <44779781+fuzzlove@users.noreply.github.com>
2021-01-03 13:17:52 -0600

    Update lazychicken.sh

--- lazychicken.sh
+++ lazychicken.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# lazychicken.sh - A lazy simple external IP check that utilizes curl with ipchicken.com :)
 #
 # Run remotely: bash -c "$(curl -fsSL https://raw.githubusercontent.com/fuzzlove/lazychicken/main/lazychicken.sh)"
 #
 
-echo "Lazychicken.sh - By: LiquidSky"
-echo
-echo "A lazy simple external IP check that utilizes ipchicken + curl"
-echo
-IP=$(curl -s https://ipchicken.com | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u)
 
-echo -e "\033[31mCurrent IP: \e[0m $IP"
+IP1=$(curl -s https://ipchicken.com | grep -oE "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | sort -u | head -1)
+IP2=$(curl -s https://icanhazip.com/)
+IP3=$(curl -s https://torguard.net/whats-my-ip.php | grep -m 2 -oE "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | sort -u | head -1)
+
+echo "[*] Checking IP from multiple sources:"
+echo
+echo -e "\033[31m [1] IP Chicken: \e[0m $IP1"
+echo
+echo -e "\033[31m [2] Icanhazip: \e[0m $IP2"
+echo
+echo -e "\033[31m [3] Torguard: \e[0m $IP3"
+echo
+echo "[!] Done"
