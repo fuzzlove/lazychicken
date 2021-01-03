ae03dbb5c3bc3ce062ae2853197d1b204c14f9ea
Joseph McPeters <44779781+fuzzlove@users.noreply.github.com>
2021-01-03 13:52:26 -0600

    Update lazychicken.sh

--- lazychicken.sh
+++ lazychicken.sh
@@ -9,12 +9,12 @@ IP1=$(curl -s https://ipchicken.com | grep -oE "(25[0-5]|2[0-4][0-9]|[01]?[0-9][
 IP2=$(curl -s https://icanhazip.com/)
 IP3=$(curl -s https://torguard.net/whats-my-ip.php | grep -m 2 -oE "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | sort -u | head -1)
 
-echo "[*] Checking External IP from multiple sources:"
+echo -e "[*] \e[1;34m Checking IP from multiple sources: \e[0m"
 echo
-echo -e "\033[31m [1] IP Chicken: \e[0m $IP1"
+echo -e "[1] \033[31m IP Chicken: \e[0m $IP1"
 echo
-echo -e "\033[31m [2] Icanhazip: \e[0m $IP2"
+echo -e "[2] \033[31m Icanhazip: \e[0m $IP2"
 echo
-echo -e "\033[31m [3] Torguard: \e[0m $IP3"
+echo -e "[3] \033[31m Torguard: \e[0m $IP3"
 echo
-echo "[!] Done"
+echo -e "[!] \e[1;34m Done \e[0m"
